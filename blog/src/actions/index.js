ddcfd55f381782fb569e35424a6131f05867a6e7
paffen <pavel.stoyanov0@hm.edu>
2022-08-01 18:40:50 +0200

    action creator make API request

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,5 +1,11 @@
-export const fetchPosts = () => {
-    return {
-        type: 'FETCH_POSTS'
+import jsonplaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPosts = async () => {
+    // Bad approach !
+   const response = await jsonplaceholder.get('/posts');
+     
+   return {
+        type: 'FETCH_POSTS',
+        payload: response
     };
-};
+}; 
