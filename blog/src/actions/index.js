041d6001bc5bb890890344c54e0cb8558c4e6b02
paffen <pavel.stoyanov0@hm.edu>
2022-08-05 23:29:07 +0200

    refactor memorize

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -7,9 +7,16 @@ export const fetchPosts = () => async dispatch => {
    dispatch({type: 'FETCH_POSTS', payload: response.data }); 
 };
 
-export const fetchUser = (id) => dispatch =>_fetchUser(id,dispatch);
-const _fetchUser = _.memoize(async(id,dispatch) => { 
+export const fetchUser = (id) => async dispatch => {
+
 const response = await jsonplaceholder.get(`/users/${id}`);
 
     dispatch({type: 'FETCH_USER', payload: response.data }); 
-});
+};
+
+// export const fetchUser = (id) => dispatch =>_fetchUser(id,dispatch);
+// const _fetchUser = _.memoize(async(id,dispatch) => { 
+// const response = await jsonplaceholder.get(`/users/${id}`);
+
+//     dispatch({type: 'FETCH_USER', payload: response.data }); 
+// });
